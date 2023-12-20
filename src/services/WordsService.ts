dfe15bf0873228eafe65491e0142c247517e3d00
Jonay Pelluz <jonay.pelluz@mad.io>
2023-12-20 20:19:06 +0100

    bugfix: changing to fetch instead of import

--- src/services/WordsService.ts
+++ src/services/WordsService.ts
@@ -5,6 +5,19 @@ import StorageService from 'src/store/StorageService';
 type SetErrorFunction = (error: Error | null) => void;
 type SetLoadingProgressFunction = (progress: number) => void;
 
+async function loadWords(start: number, end: number) {
+    try {
+      const response = await fetch(`/words_from_${start}_to_${end}.json`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching words:', error);
+    }
+  }
+  
+
 const populateWordsDB = async (
     setError: SetErrorFunction,
     setLoadingProgress: SetLoadingProgressFunction,
@@ -33,9 +46,7 @@ const populateWordsDB = async (
             const start = i * chunkSize + 1;
             const end = (i + 1) * chunkSize;
 
-            const words: string[] = await import(
-                `/words_from_${start}_to_${end}.json`
-            ).then((module) => module.default);
+            const words = await loadWords(start, end);
 
             await dbService.addWords(words);
             loadedChunks.push(i);
