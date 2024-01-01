aecf5a4d7d8450f51c62f387c5f1fa5a602d4faa
Jonay Pelluz <jonay.pelluz@mad.io>
2024-01-01 15:45:46 +0100

    feature: more basic words

--- ops/scripts/transformWordsToJSON.sh
+++ ops/scripts/transformWordsToJSON.sh
@@ -1,51 +1,64 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <filename>"
+    echo "Usage: $0 FILENAME"
     exit 1
 fi
 
-inputFile=$1
-filename=$(basename "$inputFile")
-level=${filename%%_*}
-chunkSize=100000
-offset=1
-firstElement=true
+FILE=$1
 
-declare -a generatedFiles
-
-while IFS= read -r line; do
-    if [ $((offset % chunkSize)) -eq 1 ]; then
-        if [ "$offset" -ne 1 ]; then
-            printf "]" >> "$currentOutputFile"
-            echo "File has been saved as $currentOutputFile"
-            generatedFiles+=("$currentOutputFile")
-        fi
-
-        startOffset=$offset
-        endOffset=$(($startOffset + chunkSize - 1))
-        currentOutputFile="${level}_words_from_${startOffset}_to_${endOffset}.json"
-        printf "[" > "$currentOutputFile"
-        firstElement=true
-    fi
-
-    if [ "$firstElement" = true ]; then
-        firstElement=false
-    else
-        printf "," >> "$currentOutputFile"
-    fi
-    printf "\"%s\"" "$line" >> "$currentOutputFile"
-
-    offset=$(($offset + 1))
-done < "$inputFile"
-
-if [ $(( (offset - 1) % chunkSize)) -ne 0 ]; then
-    printf "]" >> "$currentOutputFile"
-    echo "File has been saved as $currentOutputFile"
-    generatedFiles+=("$currentOutputFile")
+if [ ! -f "$FILE" ]; then
+    echo "File not found: $FILE"
+    exit 1
 fi
 
-for file in "${generatedFiles[@]}"; do
+TMP_FILE=$(mktemp)
+
+LC_COLLATE=es_ES.UTF-8 sort "$FILE" > "$TMP_FILE"
+
+mv "$TMP_FILE" "$FILE"
+
+FILE=$1
+FILENAME=$(basename "$FILE")
+LEVEL=${FILENAME%%_*}
+CHUNK_SIZE=100000
+OFFSET=1
+FIRST_ELEMENT=true
+
+declare -a GENERATED_FILES
+
+while IFS= read -r line; do
+    if [ $((OFFSET % CHUNK_SIZE)) -eq 1 ]; then
+        if [ "$OFFSET" -ne 1 ]; then
+            printf "]" >> "$CURRENT_OUTPUT_FILE"
+            echo "File has been saved as $CURRENT_OUTPUT_FILE"
+            GENERATED_FILES+=("$CURRENT_OUTPUT_FILE")
+        fi
+
+        START_OFFSET=$OFFSET
+        END_OFFSET=$(($START_OFFSET + CHUNK_SIZE - 1))
+        CURRENT_OUTPUT_FILE="${LEVEL}_words_from_${START_OFFSET}_to_${END_OFFSET}.json"
+        printf "[" > "$CURRENT_OUTPUT_FILE"
+        FIRST_ELEMENT=true
+    fi
+
+    if [ "$FIRST_ELEMENT" = true ]; then
+        FIRST_ELEMENT=false
+    else
+        printf "," >> "$CURRENT_OUTPUT_FILE"
+    fi
+    printf "\"%s\"" "$line" >> "$CURRENT_OUTPUT_FILE"
+
+    OFFSET=$(($OFFSET + 1))
+done < "$FILE"
+
+if [ $(( (OFFSET - 1) % CHUNK_SIZE)) -ne 0 ]; then
+    printf "]" >> "$CURRENT_OUTPUT_FILE"
+    echo "File has been saved as $CURRENT_OUTPUT_FILE"
+    GENERATED_FILES+=("$CURRENT_OUTPUT_FILE")
+fi
+
+for file in "${GENERATED_FILES[@]}"; do
     mv "$file" "../../public/words/"
 done
 
