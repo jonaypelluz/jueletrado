c00114fc0810e08cdb31599d717997569be0ed05
Jonay Pelluz <jonay.pelluz@mad.io>
2024-01-19 09:51:34 +0100

    feature: more words

--- src/config/LevelConfig.ts
+++ src/config/LevelConfig.ts
@@ -5,19 +5,19 @@ const LevelsConfig: LevelConfig[] = [
         level: 'beginner',
         totalChunks: 1,
         chunkSize: 100000,
-        minimumPopulatedCount: { en: 1980, es: 9086 },
+        minimumPopulatedCount: { en: 10210, es: 9086 },
     },
     {
         level: 'intermediate',
         totalChunks: 2,
         chunkSize: 100000,
-        minimumPopulatedCount: { en: 27384, es: 108764 },
+        minimumPopulatedCount: { en: 72180, es: 108764 },
     },
     {
         level: 'advanced',
         totalChunks: 7,
         chunkSize: 100000,
-        minimumPopulatedCount: { en: 177026, es: 646408 },
+        minimumPopulatedCount: { en: 312831, es: 646408 },
     },
 ];
 
