1b01433b8cf3111dc34955a4f0d131178b3b7ff2
Jonay Pelluz <jonay.pelluz@mad.io>
2026-02-22 21:19:00 +0100

    feature: update root elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,28 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import { CookieConsentProvider } from '@context/CookieContext';
 import { WordsContextProvider } from '@store/WordsContext';
 import App from 'src/components/App';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+const container = document.getElementById('root') as HTMLElement;
+
+const rootElement = (
     <StrictMode>
         <CookieConsentProvider>
             <WordsContextProvider>
                 <App />
             </WordsContextProvider>
         </CookieConsentProvider>
-    </StrictMode>,
+    </StrictMode>
 );
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container, rootElement);
+} else {
+  createRoot(container).render(rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
